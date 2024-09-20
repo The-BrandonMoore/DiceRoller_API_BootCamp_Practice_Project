dea5c96c4224ae6c9eb2803681f163b4fb0e5d7f
Brandon Moore <preacherman65@gmail.com>
2024-09-19 20:11:19 -0400

    first commit

---
+++ DiceRollerAPI_Practice_Procject/DiceRollerAPI_Practice_Procject/Controllers/DiceRollerController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+
+namespace DiceRollerAPI_Practice_Procject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiceRollerController :ControllerBase
+    {
+       static Random random = new Random();
+
+        [HttpGet("{number}")]
+
+        public string GetDieRoll(int number)
+        {
+            int dieRoll = random.Next(1, number);
+            if (dieRoll == 8 || dieRoll == 18) 
+            {
+                return $"You rolled an {dieRoll}";
+            }
+            else
+            {
+                return $"You rolled a  {dieRoll}";
+            }
+        }
+
+    }
+}
